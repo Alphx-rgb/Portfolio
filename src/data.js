ca0514ca126a9d8b231ab9d48f1658b567e5e400
Ayush Budhiraja <ayushbudhiraja795@gmail.com>
2022-04-12 20:07:45 +0530

    First Push

---
+++ src/data.js
@@ -0,0 +1,52 @@
+
+export const projects = [
+    {
+      id:1,
+      title: "Discord Bot",
+      subtitle: "MERN Stack",
+      description:
+        "Discord Bot made using NodeJs and Discord API, I built it for my cyberlabs club",
+      image: "https://www.sie.com/tachyon/sites/15/2021/05/SIE-Blog_Featured-Image_Discord.jpg?resize=1088%2C612&crop_strategy=smart",
+      link: "https://github.com/Alphx-rgb/Discord-Bot",
+    },
+    {
+      id:2,
+      title: "Web Crawler",
+      subtitle: "Python- GUI and CLI",
+      description:
+        "Reconnaissance Tool (CLI and GUI Tool) which automates the recon process on the given domain or URL and makes a report of its findings",
+      image: "http://www.codeproject.com/KB/IP/1087859/WebCrawlerImage.jpg",
+      link: "https://github.com/Alphx-rgb/Web-Crawler-GUI-Tool",
+    },
+    {
+      id:3,
+      title: "PortFolio Website",
+      subtitle: "ReactJs",
+      description:
+        "My own website showcasing my works and skills",
+      image: "http://www.codeproject.com/KB/IP/1087859/WebCrawlerImage.jpg",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      id:4,
+      title: "More Projects",
+      description:
+        "Browse for More Projects",
+      image: "./project-3.gif",
+      link: "",
+    }
+  ];
+
+export const skills =[
+  "Data Structures and Algorithms","Bash","C++","Python","HTML | CSS | SCSS","JAVASCRIPT","ReactJS","NodeJS | ExpressJS | Socket.io"
+]
+
+
+export const testimonials = [
+  {
+    name:"DifferHome",
+    quote:"HEllo",
+    image:"abc",
+    company:"DifferHome"
+  }
+]
